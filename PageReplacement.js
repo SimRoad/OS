8f9d7f0e2ce33ede5f1e38fa8812a62f503c7776
SimRoad <143764390+SimRoad@users.noreply.github.com>
2024-12-01 03:02:06 +0800

    WIP LRU

--- PageReplacement.js
+++ PageReplacement.js
@@ -2,6 +2,8 @@ let pages = [7,0,1,2,0,3,0,4,2,3,0,3,1,2,0];
 console.log(pages);
 
 console.log("Mont Capoy's Page Replacement Algorithms");
+console.log("*Disclaimer: Due to limited displaying options, there will be a letter next to the pages");
+
 FIFO(JSON.parse(JSON.stringify(pages)));
 
 function display(P, F){
@@ -27,7 +29,7 @@ function display(P, F){
 }
 
 function FIFO(P){
-    console.log("First in, First out: ");
+    console.log("\nFirst in, First out: ");
     const LIM = P.length;
     const FRAMES = 3;
 
@@ -43,7 +45,6 @@ function FIFO(P){
             hits++;
             log[FRAMES][x] = 'H';
         } else {
-            faults++;
             F[ndx] = P[x];
             log[ndx][x] = P[x];
             ndx = (ndx + 1) % FRAMES;
@@ -53,6 +54,45 @@ function FIFO(P){
             log[y].push(log[y][x]);
         }
     }
+    faults = LIM - hits;
+    console.log("Page hits: " + hits);
+    console.log("Page faults: " + faults);
+    
+    console.log("Hit Ratio = " + `(${hits} / ${LIM}) * 100 = ` + hits/LIM*100 + '%');
+    console.log("Hit Fault = " + `(${faults} / ${LIM}) * 100 = ` + faults/LIM*100 + '%');
+
+    //console.log(log)
+    display(P, log);
+}
+
+function LRU(P){
+    console.log("\nLeast Recently Used: ");
+    const LIM = P.length;
+    const FRAMES = 3;
+
+    let F = Array(FRAMES).fill(-1);
+    let uses = Array(FRAMES).fill(-1);
+    let log = Array(FRAMES).fill(null).map(() => [-1]);
+    log.push([]);
+    let x, y;
+    let ndx = 0, hits = 0, faults = 0;
+
+    for(x = 0; x < LIM; x++){
+        log[FRAMES].push('F');
+        if(F.includes(P[x])){
+            hits++;
+            log[FRAMES][x] = 'H';
+        } else {
+            F[ndx] = P[x];
+            log[ndx][x] = P[x];
+            ndx = (ndx + 1) % FRAMES;
+        }
+
+        for(y=0; y < FRAMES; y++){
+            log[y].push(log[y][x]);
+        }
+    }
+    faults = LIM - hits;
     console.log("Page hits: " + hits);
     console.log("Page faults: " + faults);
     
