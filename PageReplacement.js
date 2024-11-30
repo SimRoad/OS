7437294a5a191427efd56cd31dfe6fa210de1600
SimRoad <143764390+SimRoad@users.noreply.github.com>
2024-12-01 02:45:16 +0800

    Finished Page FIFO

--- PageReplacement.js
+++ PageReplacement.js
@@ -4,21 +4,61 @@ console.log(pages);
 console.log("Mont Capoy's Page Replacement Algorithms");
 FIFO(JSON.parse(JSON.stringify(pages)));
 
+function display(P, F){
+    const LIM = P.length;
+    const FRAMES = F.length;
+
+    let x, y;
+    let char = 97, tStr = "";
+    let table = Array(FRAMES).fill(null).map(() => ({}));
+    let temp = {};
+    for(x=0;x<FRAMES;x++){
+        table[x].Pages = `F${x+1}`;
+    }
+    table[FRAMES-1].Pages = " ";
+    
+    for(x=0;x<LIM;x++){
+        tStr = String.fromCharCode(char++);
+        for(y=0;y<FRAMES;y++){
+            table[y][`${P[x]} ${tStr}`] = F[y][x];
+        }
+    }
+    console.table(table);
+}
+
 function FIFO(P){
     console.log("First in, First out: ");
     const LIM = P.length;
+    const FRAMES = 3;
 
-    let F = [-1, -1, -1];
-    let x, ndx = 0, hits = 0, faults = 0;
+    let F = Array(FRAMES).fill(-1);
+    let log = Array(FRAMES).fill(null).map(() => [-1]);
+    log.push([]);
+    let x, y;
+    let ndx = 0, hits = 0, faults = 0;
 
     for(x = 0; x < LIM; x++){
+        log[FRAMES].push('F');
         if(F.includes(P[x])){
             hits++;
+            log[FRAMES][x] = 'H';
         } else {
             faults++;
             F[ndx] = P[x];
-            ndx = (ndx + 1) % 3;
+            log[ndx][x] = P[x];
+            ndx = (ndx + 1) % FRAMES;
+        }
+
+        for(y=0; y < FRAMES; y++){
+            log[y].push(log[y][x]);
         }
     }
-    console.log(hits + " " + faults);
+    console.log("Page hits: " + hits);
+    console.log("Page faults: " + faults);
+    
+    console.log("Hit Ratio = " + `(${hits} / ${LIM}) * 100 = ` + hits/LIM*100 + '%');
+    console.log("Hit Fault = " + `(${faults} / ${LIM}) * 100 = ` + faults/LIM*100 + '%');
+
+    //console.log(log)
+    display(P, log);
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,4 @@
-ady)));
-srtf(JSON.parse(JSON.stringify(ready)));
+for(x=0, y=1;y<=LIM;y++){
+    //     table[x].[`${y}`] = ;
+    //     x = (x+1) % FRAMES;
+    // }
